caca9e6322d25c1089e1f3cca87de88de44c7aa2
Chris Mazzochi <cmazzochi81@gmail.com>
2021-12-14 17:05:43 -0700

    improving

--- back-end/src/util/oAuthClient.js
+++ back-end/src/util/oAuthClient.js
@@ -3,5 +3,5 @@ import { google } from 'googleapis';
 export const oAuthClient = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'https://reactauth-frontend.herokuapp.com/auth/google/callback',
+    '/auth/google/callback',
 );
