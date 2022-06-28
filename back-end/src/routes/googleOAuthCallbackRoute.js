157876213d9f2332749439c82b8048a47dbde962
Chris Mazzochi <cmazzochi81@gmail.com>
2022-06-28 12:39:41 -0600

    improving

--- back-end/src/routes/googleOAuthCallbackRoute.js
+++ back-end/src/routes/googleOAuthCallbackRoute.js
@@ -1,24 +1,38 @@
-import jwt from 'jsonwebtoken';
-import { getGoogleUser} from '../util/getGoogleUser';
-import { updateOrCreateUserFromOAuth } from '../util/updateOrCreateUserFromOAuth';
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable keyword-spacing */
+/* eslint-disable padded-blocks */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable linebreak-style */
+import jwt from "jsonwebtoken";
+import { getGoogleUser } from "../util/getGoogleUser";
+import { updateOrCreateUserFromOAuth } from "../util/updateOrCreateUserFromOAuth";
 
 export const googleOAuthCallbackRoute = {
+  path: "/auth/google/callback",
+  method: "get",
+  handler: async (req, res) => {
+    const { code } = req.query;
+    const oAuthUserInfo = await getGoogleUser({ code });
+    const updatedUser = await updateOrCreateUserFromOAuth({ oAuthUserInfo });
+    const { _id: id, isVerified, email, info } = updatedUser;
 
-    path: '/auth/google/callback',
-    method: 'get',
-    handler: async (req, res) => {
-
-        const { code } = req.query;
-        const oAuthUserInfo = await getGoogleUser({ code });
-        const updatedUser = await updateOrCreateUserFromOAuth({oAuthUserInfo });
-        const { _id: id, isVerified, email, info } = updatedUser;
-
-        jwt.sign(
-            { id, isVerified, email, info },
-            process.env.JWT_SECRET,
-            (err, token) => {
-                if(err) return res.sendStatus(500);
-                res.redirect(`https://reactauth-frontend.herokuapp.com/login?token=${token}`);
-        });
-    }
-}
+    jwt.sign(
+      { id, isVerified, email, info },
+      process.env.JWT_SECRET,
+      (err, token) => {
+        if (err) return res.sendStatus(500);
+        res.redirect(
+          `https://reactauth-frontend.herokuapp.com/login?token=${token}`
+        );
+      }
+    );
+  },
+};
--- back-end/src/util/oAuthClient.js
+++ back-end/src/util/oAuthClient.js
@@ -1,7 +1,16 @@
-import { google } from 'googleapis';
+/* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-spacing */
+import { google } from "googleapis";
 
 export const oAuthClient = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    'https://reactauth-backend.herokuapp.com/auth/google/callback',
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "/auth/google/callback"
 );
