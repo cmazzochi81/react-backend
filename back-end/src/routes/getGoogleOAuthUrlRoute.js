d1539a5d6b91f068956ed980f0a9a354a9bfe232
Chris Mazzochi <cmazzochi81@gmail.com>
2022-06-28 13:02:38 -0600

    improving

--- back-end/src/routes/getGoogleOAuthUrlRoute.js
+++ back-end/src/routes/getGoogleOAuthUrlRoute.js
@@ -1,10 +1,18 @@
-import { getGoogleOAuthUrl } from '../util/getGoogleOAuthUrl';
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-spacing */
+import { getGoogleOAuthUrl } from "../util/getGoogleOAuthUrl";
 
 export const getGoogleOAuthUrlRoute = {
-    path: '/auth/google/url',
-    method: 'get',
-    handler: (req, res) => {
-        const url = getGoogleOAuthUrl();
-        res.status(200).json({ url });
-    }
+  path: "/auth/google/url",
+  method: "get",
+  handler: (req, res) => {
+    const url = getGoogleOAuthUrl();
+    res.status(200).json({ url });
+  },
 };
