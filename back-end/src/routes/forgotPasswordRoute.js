5f5ab0181ee2cae92f97df0e16fb0c87e8fe6cb3
Chris Mazzochi <cmazzochi81@gmail.com>
2022-06-28 18:40:32 -0600

    improving

--- back-end/src/routes/forgotPasswordRoute.js
+++ back-end/src/routes/forgotPasswordRoute.js
@@ -27,7 +27,7 @@ export const forgotPasswordRoute = {
           subject: "Password Reset",
           text: `
                             To reset your password, click this link: 
-                            https://reactauth-frontend.herokuapp.com/reset-password/
+                            https://reactauth-frontend.herokuapp.com/reset-password/${passwordResetCode}
 
 
                         `,
